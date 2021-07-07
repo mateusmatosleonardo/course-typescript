3ea0686eb4d5ac056b53221a6c0d5536db5b6ff3
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-07 20:22:53 -0300

    updating

--- class-one/app.js
+++ class-one/app.js
@@ -8,3 +8,12 @@ console.log(mensage);
 // ===> Arrays
 var person = ['Mateus', 'Luca', 'Vini'];
 console.log(person[1]);
+// ===> Object
+var student;
+student = { name: 'Mateus', age: 21, "class": 2 };
+console.log(student);
+// ===> Function
+function multNum(num1, num2) {
+    return num1 * num2;
+}
+console.log(multNum(4, 20));
--- class-one/app.ts
+++ class-one/app.ts
@@ -11,3 +11,20 @@ console.log(mensage);
 // ===> Arrays
 let person: string[] = ['Mateus', 'Luca', 'Vini'];
 console.log(person[1]);
+
+// ===> Object
+let student:{
+    name: string;
+    age: number;
+    class: number;
+};
+
+student = { name: 'Mateus', age: 21, class: 2}
+console.log(student);
+
+// ===> Function
+function multNum(num1: number, num2:number){
+    return num1 * num2;
+}
+
+console.log(multNum(4,20));
