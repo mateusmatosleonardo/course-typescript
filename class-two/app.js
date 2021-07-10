d192f810978aac45f9def1a2c3bed2ef25984416
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-09 23:37:22 -0300

    updating

---
+++ class-two/app.js
@@ -0,0 +1,14 @@
+// ========== TYPE BOOLEAN ========== //
+//           ALERT !!!          //
+/* Boolean != boolean
+   Boolean: type object
+   boolean: type: primitive
+
+   recommendable: boolean for applications
+*/
+// => example one
+var taskCompleted = true;
+var pendingTtask = false;
+console.log(taskCompleted);
+console.log(pendingTtask);
+// => example two
