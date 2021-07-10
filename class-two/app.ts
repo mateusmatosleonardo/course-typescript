cfe83cc54c6fb475f796afc04e3b073a98c142b9
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-09 23:27:58 -0300

    updating

--- class-two/app.ts
+++ class-two/app.ts
@@ -1,2 +1,9 @@
 // ========== TYPE BOOLEAN ========== //
 
+//           ALERT !!!          //
+/* Boolean != boolean
+   Boolean: type object
+   boolean: type: primitive
+
+   recommendable: boolean for applications
+*/
