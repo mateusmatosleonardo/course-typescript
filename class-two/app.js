02b0020a5bcfc9b6edb160780c55f67c0169d8a2
mateusmatosleonardo <m4teusmatos@gmail.com>
2021-07-09 23:42:36 -0300

    updating

--- class-two/app.js
+++ class-two/app.js
@@ -12,3 +12,10 @@ var pendingTtask = false;
 console.log(taskCompleted);
 console.log(pendingTtask);
 // => example two
+var done = false;
+if (done) {
+    console.log("The task has been completed!!!");
+}
+else {
+    console.log("Task not completed");
+}
